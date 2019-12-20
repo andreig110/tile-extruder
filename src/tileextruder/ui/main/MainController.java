5f37b90f4061de8c5ed01a48f3fa1aecd185f05d
Andrei Guga <icandreig@gmail.com>
2019-12-20 22:05:10 +0200

    UI: Add ChoiceBox control to zoom the image (tileset)

--- src/tileextruder/ui/main/MainController.java
+++ src/tileextruder/ui/main/MainController.java
@@ -1,16 +1,14 @@
 package tileextruder.ui.main;
 
 import javafx.application.Platform;
+import javafx.beans.value.ObservableValue;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TextFormatter;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
@@ -53,8 +51,37 @@ public class MainController {
     @FXML // fx:id="extrudeBtn"
     private Button extrudeBtn;
 
+    @FXML // fx:id="zoomChoiceBox"
+    private ChoiceBox<String> zoomChoiceBox;
+
     private FileChooser fileChooser;
     private BufferedImage bufferedImage;
+    private int zoomFactor = 100;
+    private boolean controlsDisabled = true;
+
+    @FXML
+    void initialize() {
+        fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("PNG Files", "*.png"));
+
+        UnaryOperator<TextFormatter.Change> tileSizeFilter = change -> {
+            String newText = change.getControlNewText();
+            if (newText.matches("([1-9][0-9]*)?")) {
+                if ((newText.length() < 4) || (Integer.parseInt(newText) <= 1024)) {
+                    return change;
+                }
+            }
+            return null;
+        };
+        tileWidthText.setTextFormatter(new TextFormatter<>(tileSizeFilter));
+        tileHeightText.setTextFormatter(new TextFormatter<>(tileSizeFilter));
+
+        zoomChoiceBox.getSelectionModel().selectedItemProperty()
+                .addListener( (ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+                    zoomFactor = Integer.parseInt( newValue.substring(0, newValue.length() - 2) );
+                    updateImageView();
+                });
+    }
 
     @FXML
     void about(ActionEvent event) throws IOException {
@@ -90,17 +117,33 @@ public class MainController {
 
         updateImageView();
 
-        // Enable "extrude" & "save" controls
-        saveMenuItem.setDisable(false);
-        saveBtn.setDisable(false);
-        rightPanel.setDisable(false);
+        // Enable controls if disabled
+        if (controlsDisabled) {
+            saveMenuItem.setDisable(false);
+            saveBtn.setDisable(false);
+            rightPanel.setDisable(false);
+            zoomChoiceBox.setDisable(false);
+            controlsDisabled = false;
+        }
     }
 
     private void updateImageView() {
         Image fxImage = SwingFXUtils.toFXImage(bufferedImage, null);
-        imageView.setImage(FXImageUtil.scaleImage(fxImage, 4));
-        imageView.setFitWidth(4 * bufferedImage.getWidth());
-        imageView.setFitHeight(4 * bufferedImage.getHeight());
+        final double scaleFactor = zoomFactor / 100d;
+
+        switch (zoomFactor) {
+            case 25:
+            case 50:
+            case 100:
+                imageView.setImage(fxImage);
+                break;
+            case 200:
+            case 400:
+                imageView.setImage(FXImageUtil.scaleImage(fxImage, (int) scaleFactor));
+        }
+
+        imageView.setFitWidth(scaleFactor * bufferedImage.getWidth());
+        imageView.setFitHeight(scaleFactor * bufferedImage.getHeight());
     }
 
     @FXML
@@ -128,25 +171,6 @@ public class MainController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    @FXML
-    void initialize() {
-        fileChooser = new FileChooser();
-        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("PNG Files", "*.png"));
-
-        UnaryOperator<TextFormatter.Change> tileSizeFilter = change -> {
-            String newText = change.getControlNewText();
-            if (newText.matches("([1-9][0-9]*)?")) {
-                if ((newText.length() < 4) || (Integer.parseInt(newText) <= 1024)) {
-                    return change;
-                }
-            }
-            return null;
-        };
-
-        tileWidthText.setTextFormatter(new TextFormatter<>(tileSizeFilter));
-        tileHeightText.setTextFormatter(new TextFormatter<>(tileSizeFilter));
     }
 
 }
