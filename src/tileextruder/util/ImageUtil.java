06da55263bba8ac6258be55db0fdc90904794fbe
Andrei Guga <icandreig@gmail.com>
2019-12-25 23:12:03 +0200

    Add support for 16 bpc PNG formats
    16 bpc PNG formats:
    
    - 16 bpc RGB
    - 16 bpc RGBA
    - 16 bpc GREY
    - 16 bpc GREYA (grey + alpha)

--- src/tileextruder/util/ImageUtil.java
+++ src/tileextruder/util/ImageUtil.java
@@ -1,8 +1,6 @@
 package tileextruder.util;
 
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBuffer;
-import java.awt.image.DataBufferByte;
+import java.awt.image.*;
 
 public class ImageUtil {
 
@@ -14,18 +12,26 @@ public class ImageUtil {
                 if (src.getType() == BufferedImage.TYPE_BYTE_BINARY) {  // byte-packed 1, 2, or 4 bit image
                     // TODO
                 } else {
-                    // bpc = bits per component (channel)
-                    // TYPE_BYTE_INDEXED - indexed byte image (8 bit)
-                    // TYPE_3BYTE_BGR    - 8 bpc RGB
-                    // TYPE_4BYTE_ABGR   - 8 bpc RGBA
-                    // TYPE_BYTE_GRAY    - 8 bpc GRAY
-                    // TYPE_CUSTOM       - 8 bpc GRAYA (grey + alpha)
+                    /* else if src.getType() is one of the following image type:
+                       TYPE_BYTE_INDEXED - indexed byte image (8 bit)
+                       TYPE_3BYTE_BGR    - 8 bpc RGB
+                       TYPE_4BYTE_ABGR   - 8 bpc RGBA
+                       TYPE_BYTE_GRAY    - 8 bpc GRAY
+                       TYPE_CUSTOM       - 8 bpc GRAYA (grey + alpha)
+                       bpc = bits per component (channel)
+                     */
                     copyRectDataBufferByte(src, srcRectX, srcRectY, srcRectWidth, srcRectHeight, dest, destPosX, destPosY);
                 }
                 break;
-            case DataBuffer.TYPE_USHORT:
             case DataBuffer.TYPE_SHORT:
-                // TODO
+            case DataBuffer.TYPE_USHORT:
+                /* BufferedImage image type for 16 bpc PNG formats:
+                   TYPE_CUSTOM      - 16 bpc RGB
+                   TYPE_CUSTOM      - 16 bpc RGBA
+                   TYPE_USHORT_GRAY - 16 bpc GREY
+                   TYPE_CUSTOM      - 16 bpc GREYA
+                 */
+                copyRectDataBufferShort(src, srcRectX, srcRectY, srcRectWidth, srcRectHeight, dest, destPosX, destPosY);
         }
     }
 
@@ -46,6 +52,30 @@ public class ImageUtil {
         for (int y = srcRectY;
                  y < srcRectY + srcRectHeight;
                  y++, srcOffset += n * src.getWidth(), destOffset += n * dest.getWidth()) {
+            System.arraycopy(srcBuf, srcOffset, destBuf, destOffset, n * srcRectWidth);
+        }
+    }
+
+    private static void copyRectDataBufferShort(final BufferedImage src, final int srcRectX, final int srcRectY,
+                                                final int srcRectWidth, final int srcRectHeight,
+                                                final BufferedImage dest, final int destPosX, final int destPosY) {
+        short[] srcBuf;
+        short[] destBuf;
+        if (src.getRaster().getDataBuffer() instanceof DataBufferUShort) {
+            srcBuf = ((DataBufferUShort) src.getRaster().getDataBuffer()).getData();
+            destBuf = ((DataBufferUShort) dest.getRaster().getDataBuffer()).getData();
+        } else {
+            srcBuf = ((DataBufferShort) src.getRaster().getDataBuffer()).getData();
+            destBuf = ((DataBufferShort) dest.getRaster().getDataBuffer()).getData();
+        }
+
+        final int n = src.getColorModel().getNumComponents();  // the number of components, including alpha
+        int srcOffset = n * (srcRectY * src.getWidth() + srcRectX);
+        int destOffset = n * (destPosY * dest.getWidth() + destPosX);
+
+        for (int y = srcRectY;
+             y < srcRectY + srcRectHeight;
+             y++, srcOffset += n * src.getWidth(), destOffset += n * dest.getWidth()) {
             System.arraycopy(srcBuf, srcOffset, destBuf, destOffset, n * srcRectWidth);
         }
     }
